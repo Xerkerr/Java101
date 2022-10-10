72ee986522ece8c5d198b806837db010bdbc6306
BERKER <101291907+Xerkerr@users.noreply.github.com>
2022-10-10 14:32:59 +0300

    Add files via upload

---
+++ Takstimetre.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        int km;
+        double perKm = 2.20, startPrice = 10, total;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("mesafeyi km olarak gir :"  );
+        km = input.nextInt();
+
+        total = (km * perKm);
+        total += startPrice;
+
+        System.out.println("giren ücret : " + total);
+
+
+
+    }
+}
