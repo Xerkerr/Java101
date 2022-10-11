e7805803c815c0263ecfd046503515827bca600e
BERKER <101291907+Xerkerr@users.noreply.github.com>
2022-10-11 16:33:22 +0300

    Add files via upload

---
+++ kullanicigirisi.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        String username, password;
+
+    Scanner inp = new Scanner(System.in);
+
+    System.out.println("kullanici adiniz ");
+    username= inp.nextLine();
+
+    System.out.println("sifreniz ");
+    password = inp.nextLine();
+
+    if (username.equals("patika") && (password.equals("123213") ));
+        System.out.println("sokuldun !" );
+
+    }
+
+
+}
