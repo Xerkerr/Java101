c116e9ffbb4c7f414dcb8e0850413058024225e0
BERKER <101291907+Xerkerr@users.noreply.github.com>
2022-10-16 16:55:03 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,36 +1,22 @@
-import java.util.Scanner;
+import java.util.Scanner ;
 
 public class Main {
     public static void main(String[] args) {
-        int n1, n2, sec;
-
-
         Scanner input = new Scanner(System.in);
-        System.out.print("ilk sayını gir : ");
-        n1 = input.nextInt();
-        System.out.print("ikinci sayını gir : ");
-        n2 = input.nextInt();
 
-        System.out.println("1-topla\n 2-çıkar\n 3-böl\n 4-çarp");
-        System.out.println("seçiminiz : ");
-        sec = input.nextInt();
+        int n,k ;
 
+        System.out.print("Enter the bottom number : ");
+        n = input.nextInt();
 
-        switch (sec) {
-            case 1:
-                System.out.println("Sonucunuz " + (n1+n2));
-                break;
-            case 2:
-                System.out.println("Sonucunuz " + (n1-n2));
-                break;
-            case 3:
-                System.out.println("Sonucunuz " + (n1/n2));
-                break;
-            case 4:
-                System.out.println("Sonucunuz " + (n1*n2));
-                break;
+        System.out.print("Enter the exponent : ");
+        k = input.nextInt();
 
+        int total = 1 ;
+
+        for (int i = 1 ; i <= k ; i++){
+            total *= n ;
         }
-    }}
-
-
+        System.out.println(n + "^" + k + " : " + total);
+    }
+}
