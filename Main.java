6ae9ea9689eaec9d37413f9efd363cf7de146d38
BERKER <101291907+Xerkerr@users.noreply.github.com>
2022-11-16 12:56:08 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    static void f(int n){
+        System.out.print(n+" ");
+        if(n>0){
+            f(n-5);
+            System.out.print(n+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz:  ");
+        int n=input.nextInt();
+        f(n);
+        input.close();
+    }
+}
